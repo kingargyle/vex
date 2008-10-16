4f988bd0301cb332369bec23d235bcbac011d0a9
dacarver <dacarver>
2008-10-16 22:08:00 +0000

    bug 251134 - VEX Unit Tests Failing.

--- sourceediting/tests/org.eclipse.wst.xml.vex.core.tests/src/org/eclipse/wst/xml/vex/core/internal/layout/TestStaticTextBox.java
+++ sourceediting/tests/org.eclipse.wst.xml.vex.core.tests/src/org/eclipse/wst/xml/vex/core/internal/layout/TestStaticTextBox.java
@@ -12,6 +12,8 @@ package org.eclipse.wst.xml.vex.core.internal.layout;
 
 import java.net.URL;
 
+import org.eclipse.wst.xml.vex.core.internal.core.DisplayDevice;
+import org.eclipse.wst.xml.vex.core.internal.css.MockDisplayDevice;
 import org.eclipse.wst.xml.vex.core.internal.css.StyleSheet;
 import org.eclipse.wst.xml.vex.core.internal.css.StyleSheetReader;
 import org.eclipse.wst.xml.vex.core.internal.css.Styles;
@@ -28,38 +30,43 @@ public class TestStaticTextBox extends TestCase {
 
 	FakeGraphics g;
 	LayoutContext context;
+	RootElement root = new RootElement("root");
+	Styles styles;
 
-	public TestStaticTextBox() throws Exception {
-
+	@Override
+	protected void setUp() throws Exception {
+		// TODO Auto-generated method stub
+		super.setUp();
+		DisplayDevice.setCurrent(new MockDisplayDevice(90, 90));
 		URL url = this.getClass().getResource("test.css");
 		StyleSheetReader reader = new StyleSheetReader();
 		StyleSheet ss = reader.read(url);
 
-		this.g = new FakeGraphics();
+		g = new FakeGraphics();
 
-		this.context = new LayoutContext();
-		this.context.setBoxFactory(new CssBoxFactory());
-		this.context.setGraphics(this.g);
-		this.context.setStyleSheet(ss);
+		context = new LayoutContext();
+		context.setBoxFactory(new CssBoxFactory());
+		context.setGraphics(this.g);
+		context.setStyleSheet(ss);
+
+		styles = context.getStyleSheet().getStyles(root);
+		
+	}
+	
+	public TestStaticTextBox() throws Exception {
+
 	}
 
 	public void testSplit() throws Exception {
-		RootElement root = new RootElement("root");
-
-		Styles styles = this.context.getStyleSheet().getStyles(root);
 
 		int width = g.getCharWidth();
-
-		// 0 6 13 21
-		// / / / /
-		// baggy orange trousers
 
 		StaticTextBox box = new StaticTextBox(this.context, root,
 				"baggy orange trousers");
 		assertEquals(box.getText().length() * width, box.getWidth());
 		assertEquals(styles.getLineHeight(), box.getHeight());
-		assertSplit(box, 22, false, "baggy orange trousers", null);
-		assertSplit(box, 21, false, "baggy orange trousers", null);
+		assertSplit(box, 22, false, "baggy orange ", "trousers");
+		assertSplit(box, 21, false, "baggy orange ", "trousers");
 		assertSplit(box, 20, false, "baggy orange ", "trousers");
 		assertSplit(box, 13, false, "baggy orange ", "trousers");
 		assertSplit(box, 12, false, "baggy ", "orange trousers");
@@ -69,58 +76,30 @@ public class TestStaticTextBox extends TestCase {
 		assertSplit(box, 0, false, null, "baggy orange trousers");
 		assertSplit(box, -1, false, null, "baggy orange trousers");
 
-		assertSplit(box, 22, true, "baggy orange trousers", null);
-		assertSplit(box, 21, true, "baggy orange trousers", null);
+		assertSplit(box, 22, true, "baggy orange ", "trousers");
+		assertSplit(box, 21, true, "baggy orange ", "trousers");
 		assertSplit(box, 20, true, "baggy orange ", "trousers");
 		assertSplit(box, 13, true, "baggy orange ", "trousers");
 		assertSplit(box, 12, true, "baggy ", "orange trousers");
 		assertSplit(box, 6, true, "baggy ", "orange trousers");
-		assertSplit(box, 5, true, "baggy", " orange trousers");
+		assertSplit(box, 5, true, "baggy ", "orange trousers");
 		assertSplit(box, 4, true, "bagg", "y orange trousers");
 		assertSplit(box, 3, true, "bag", "gy orange trousers");
 		assertSplit(box, 2, true, "ba", "ggy orange trousers");
 		assertSplit(box, 1, true, "b", "aggy orange trousers");
 		assertSplit(box, 0, true, "b", "aggy orange trousers");
 		assertSplit(box, -1, true, "b", "aggy orange trousers");
-
-		// 0 5 10
-		// / / /
-		box = new StaticTextBox(this.context, root, "red  green");
-		assertSplit(box, 11, false, "red  green", null);
-		assertSplit(box, 10, false, "red  green", null);
+	}
+	
+	public void testSpaceSplit() throws Exception {
+		StaticTextBox box = new StaticTextBox(context, root, "red  green");
+		assertSplit(box, 11, false, "red  ", "green");
+		assertSplit(box, 10, false, "red  ", "green");
 		assertSplit(box, 9, false, "red  ", "green");
 		assertSplit(box, 5, false, "red  ", "green");
-
-		//
-		// This is the way it should work from a formatting point-of-view, but
-		// it could be problematic when it gets to positioning the caret, e.g.
-		// if we had lots of spaces to the right of a word it would format
-		// properly, but the caret would get carried out of the formatted area.
-		// 
-		// assertSplit(box, 4, false, null, "red  green");
-		// assertSplit(box, 1, false, null, "red  green");
-		// assertSplit(box, 0, false, null, "red  green");
-		// assertSplit(box, -1, false, null, "red  green");
-
-		//
-		// This solves the caret problem at the expense of the formatting
-		// problem. It also happens to be how my initial implementation works!
-		// In the end it doesn't much matter, since Vex should collapse
-		// contiguous space into a single space character.
-		//
-		assertSplit(box, 4, false, "red ", " green");
-		assertSplit(box, 3, false, null, "red  green");
-		assertSplit(box, 1, false, null, "red  green");
-		assertSplit(box, 0, false, null, "red  green");
-		assertSplit(box, -1, false, null, "red  green");
-
-		assertSplit(box, 4, true, "red ", " green");
-		assertSplit(box, 3, true, "red", "  green");
-		assertSplit(box, 1, true, "r", "ed  green");
-		assertSplit(box, 0, true, "r", "ed  green");
-		assertSplit(box, -1, true, "r", "ed  green");
-
+		
 	}
+	
 
 	private void assertSplit(StaticTextBox box, int splitPos, boolean force,
 			String left, String right) {
